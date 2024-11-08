f1b1713a6f87f1cc8229b28696c88f2ed669da66
Samuel Jeffman <samuel.jeffman@initgroup.com>
2024-11-08 13:04:22 +0100

    Added PrintCalendar().

--- functions.h
+++ functions.h
@@ -5,5 +5,6 @@ int getLeapYear(int year);
 int numberOfDaysSinceYearOne(int year);
 int getLeapDays(int year);
 void printYear(int year, char option[]);
+void printCalendar(int totalDaysSinceYearOne, int startMonth, int endMonth, int isLeapYear, const char* months[], const char* weekdays[], int year, char option[]);
 
 #endif // FUNCTIONS
