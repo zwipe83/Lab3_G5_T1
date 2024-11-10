54c04113d6c0238932580980edfb84b1c6f32ca7
Samuel Jeffman <sjeffman83@outlook.com>
2024-11-10 20:39:59 +0100

    Updated prototypes

--- tests.h
+++ tests.h
@@ -4,9 +4,9 @@
 int getLeapYear(int year);
 int getLeapDays(int year);
 int numberOfDaysSinceYearOne(int year);
-void printYear(int year, char option[]);
-void printMonthNames(const char* months[], int startMonth, int endMonth, char option[]);
-void printWeekdayNames(const char* weekdays[], char option[]);
+void printYear(int year, const char option[]);
+void printMonthNames(const char* months[], int startMonth, int endMonth, const char option[]);
+void printWeekdayNames(const char* weekdays[], const char option[]);
 int getWeekNumber(int year, int month, int day);
 void printWeekNumber(int year, int startMonth, int month, const	int* dayCount, int nextDay);
 int* getFirstDayInMonths(int startMonth, int endMonth, int totalDays, int isLeapYear);
